08597032cb4449940d62ec0067fd65f5ee1aadcd
komalpreet1234 <200429219@student.georgianc.on.ca>
2020-08-07 09:35:41 +0530

    Lab

---
+++ client/src/components/blogs/Edit.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Container } from 'react-bootstrap';
+import Axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const Edit = function (props) {
+
+  const id = props.location.state.id;
+
+  const [inputs, setInputs] = useState({
+    title: '',
+    content: '',
+    status: 'DRAFT'
+  });
+
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const blogResp = await Axios.get('/blogs/${id}')
+      if (blogResp.status == 200) setInputs(blogResp.data);
+    })();
+  }, [])
+
+  const handleSubmit = async event => {
+    //prevent the system sent to the page
+    event.preventDefault();
+
+    try {
+      //redirect the page to blogs
+      const resp = await Axios.post('/api/blogs', inputs);
+
+      //200 means operate successfullysss 
+      if (resp.status === 200)  {
+        toast("The blog was updated successfully", {
+          type: toast.TYPE.SUCCESS
+        });
+        setRedirect(true);
+      }
+    } catch (error) {
+      toast("There was an issue updating the blog", {
+        type: toast.TYPE.ERROR
+      });
+    }
+  };
+
+  const handleInputChange = async event => {
+    event.persist();
+
+    const { name, value } = event.target;
+
+    setInputs(inputs => ({
+      ...inputs,
+      [name]: value
+    }));
+  };
+
+  if (redirect) return (<Redirect to="/blogs"/>);
+
+  return (
+    <Container className="my-5">
+      <header>
+        <h1>Edit Blog Post</h1>
+      </header>
+
+      <hr/>
+
+      <div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Title:</Form.Label>
+            <Form.Control
+              name="title"
+              onChange={handleInputChange}
+              value={inputs.title}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Content:</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="content"
+              onChange={handleInputChange}
+              value={inputs.content}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Status:</Form.Label>
+            <Form.Control
+              as="select"
+              name="status"
+              onChange={handleInputChange}
+              defaultValue={inputs.status || 'DRAFT'}
+            >
+              <option value="DRAFT">draft</option>
+              <option value="PUBLISHED">published</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <button type="submit" className="btn btn-primary">Create</button>
+          </Form.Group>
+        </Form>
+      </div>
+    </Container>
+  );
+
+};
+
+export default Edit;
---
+++ client/src/components/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function About () {
+  return (
+    <Container className="my-5">
+      <header class="jumbotron">
+        <h1>About Me</h1>
+      </header>
+
+      <div>
+        <p>The autobiography of Komalpreet</p>
+      </div>
+    </Container>
+  );
+}
+
+export default About;
